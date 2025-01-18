81734af60058a15b87f75d54babd2151f6ce12de
phucthemen <phucthemen@gmail.com>
2025-01-18 13:15:34 +0700

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Test suite for the LFT Framework.
+"""
+
+import os
+import sys
+
+# Add the src directory to Python path for testing
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
